6735d7bb845114ccd6211b773bc5c07499bda643
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-05 03:18:36 +0530

    created user db

---
+++ server/controllers/userController.js
@@ -0,0 +1,35 @@
+const User = require("../models/userModel");
+const asyncHandler=require("express-async-handler");
+const getUser = asyncHandler(async(req,res)=>{
+        const users = await User.find(req.params.id);
+        res.json(users);
+    }
+)
+const createUser=asyncHandler(
+    async(req,res)=>{
+ 
+       
+        const {firstName,lastName,gender,age,date,city,state,country,message} =req.body;
+                 if(!firstName || !lastName || !gender || !age || !date || !city || !state || !country ){
+                   res.status(400);
+                  throw new Error("Please fill all the fields");
+            }
+        else{
+            const user = new User(
+                                {firstName:req.body.firstName,
+                                lastName:req.body.lastName,
+                                gender:req.body.gender,
+                                age:req.body.age,
+                                date:req.body.date,
+                                city:req.body.city,
+                                state:req.body.state,
+                                country:req.body.country,
+                                message:req.body.message,
+                                id:req.body.id});
+                            const createdUser = await user.save();
+                            res.json(createdUser);
+            
+        }
+    }
+)
+module.exports={getUser,createUser};
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,10 @@
-const express = require('require')
-const router = express.Router()
-router.route('/userprofile').post()
-module.exports = router;
+const express = require('express');
+const { getUser,createUser} = require('../controllers/userController');
+const router = express.Router();
+
+
+router.route('/').get(getUser)
+router.route('/newuser').post(createUser);
+// router.route('/:id').get(getNoteById).put(updateNote).delete(deleteNote);
+
+module.exports=router;
