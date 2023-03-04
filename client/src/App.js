57d80712bc62b362c1462b44dd8e365c3575ff1f
RacHiT101 <rachitgala05@gmail.com>
2023-03-04 20:04:09 +0530

    no changes

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <Router>
       <Navbar />
-
+      
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
