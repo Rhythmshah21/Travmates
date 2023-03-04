a5449a977f84633dfe89910aa37fda6e354ed3f3
unknown <mahekmj123@gmail.com>
2023-03-04 16:39:21 +0530

    test

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ const mongoose = require('mongoose')
 
 const app = express();
 const uri = process.env.MONGO_URI;
-
+const userRoutes = require('./routes/userRoutes')
 mongoose
   .connect(uri, { useNewUrlParser: true, useCreateIndex: true })
   .
 
 app.use(cors());
-
+app.use('/api/users',userRoutes)
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
