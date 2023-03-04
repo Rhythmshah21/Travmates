050dc143cb779ba2af6de6e0d1abd1f4ad660e57
Priyanka Ramachandran <111295843+Priyankaa2503@users.noreply.github.com>
2023-03-05 02:25:44 +0530

    modified footer and added about us page

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -11,31 +11,37 @@ const Blogs = () => {
  
   const blogs = [
     {
+      "id":1,
       "title": "Exploring the Hidden Gems of Tokyo",
       "content": "Tokyo is a vibrant city with countless sights and sounds to explore. However, there are also many hidden gems that are off the beaten path. In this blog post, we'll share some of the best hidden parks, local restaurants, and unique experiences to have in Tokyo. For example, you can visit the beautiful and serene Shinjuku Gyoen National Garden, or try some delicious and authentic Japanese street food at the Tsukiji Outer Market. We'll also provide tips for getting around the city and avoiding the crowds.",
       "image": "/images/hiddengems.jpeg"
     },
     {
+      "id":2,
       "title": "Hiking in the Swiss Alps",
       "content": "The Swiss Alps are a hiker's paradise, with stunning views and challenging trails. In this blog post, we'll share some of the best hikes in the Swiss Alps, including the Haute Route and the Matterhorn. We'll also provide tips for planning your trip, such as what gear to bring and where to stay. Additionally, we'll recommend some lesser-known trails for those who want to avoid the crowds and experience the beauty of the Alps in solitude.",
       "image": "/images/Swiss.jpeg"
     },
     {
+      "id":3,
       "title": "Finding Adventure in Costa Rica",
       "content": "Costa Rica is a beautiful country with diverse landscapes and plenty of adventure opportunities. In this blog post, we'll share some of the best adventures to have in Costa Rica, from zip-lining through the jungle to surfing on the Pacific coast. We'll also provide tips for planning your trip, including the best time to visit and what to pack. Furthermore, we'll recommend some off-the-beaten-path activities for those who want to explore the country beyond the usual tourist hotspots.",
       "image": "/images/Costa.jpeg"
     },
     {
+      "id":4,
       "title": "A Road Trip Through the American Southwest",
       "content": "The American Southwest is a vast and diverse region with stunning natural beauty and fascinating history. In this blog post, we'll share some of the best road trip routes through the Southwest, such as the iconic Route 66 and the scenic Pacific Coast Highway. We'll also recommend some unique stops along the way, such as the ancient cliff dwellings at Mesa Verde National Park or the quirky roadside attractions of the Mojave Desert.",
       "image": "images/Southwest.jpeg"
     },
     {
+      "id":5,
       "title": "Cruising the Mediterranean",
       "content": "A Mediterranean cruise is a luxurious way to see some of the world's most beautiful destinations, from Italy to Greece to Spain. In this blog post, we'll provide an overview of what to expect on a Mediterranean cruise, including the best time to go and what to pack. We'll also highlight some of the most popular ports of call, such as the ancient city of Rome or the stunning Greek island of Santorini.",
       "image": "/images/Manarola.jpeg"
     },
     {
+      "id":6,
       "title": "Sailing the Caribbean",
       "content": "The Caribbean is a popular destination for sailing enthusiasts, with crystal-clear waters and plenty of secluded coves to explore. In this blog post, we'll share tips for planning your Caribbean sailing trip, including what type of boat to rent and what to pack. We'll also recommend some of the best islands to visit, such as the vibrant and colorful St. Lucia or the secluded and serene British Virgin Islands",
       "image": "/images/Carribean.jpeg"
@@ -45,9 +51,9 @@ const Blogs = () => {
 
 
 
-  console.log(data);
+
   return (
-    <section class="text-gray-400 bg-gray-900 font-playflair z-0">
+    <section class="text-gray-400 bg-gray-900 font-playflair z-0 p-24">
       {loading ? (
         <div className="h-96 flex w-full justify-center items-center bg-gray-900">
           <TailSpin height={40} color="white" />
@@ -56,27 +62,26 @@ const Blogs = () => {
         <>
           <div class="container px-5 mx-auto gap-y-20">
             <div class="flex flex-wrap justify-center">
-              {blogs.map((data) => (
-                
+              {blogs.map((blogs) => {
+                return (
                   <motion.div
                     initial={{ scale: 0, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1, y: [300,0,0,80] }}
                     transition={{ type: "spring", duration: 2 }}
-                    key={data.id}
+                    key={blogs.id}
                     class="p-4 lg:w-1/2"
                   >
-                    <div class="h-full bg-gray-800 bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                      <h2 class="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
+                    <div class="h-full bg-gray-800 shadow-xl bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                    <img src={blogs.image} className='w-[652px] h-[360px] p-4'></img>
+                      <h2 class="tracking-widest text-md title-font font-medium text-gray-500 mb-1">
                         {blogs.title}
                       </h2>
                       {/* <img src={data.image_link} alt="default" className="mx-auto p-3"/> */}
                       <h1 class="title-font sm:text-2xl text-xl font-medium text-white mb-3">
                         {data.name}
                       </h1>
-                      <p class="leading-relaxed mb-3">{data.description}</p>
-                      <p class="leading-relaxed mb-3 sm:text-2xl text-xl mr-5">
-                        $ {data.price}
-                      </p>
+                      <p class="leading-relaxed mb-3">{blogs.content}</p>
+                    
                       <a
                         href={data.product_link}
                         class="text-indigo-400 inline-flex items-center"
@@ -128,8 +133,8 @@ const Blogs = () => {
                       </div>
                     </div>
                   </motion.div>
-                
-              ))}
+                );
+              })}
             </div>
           </div>
         </>
--- server/server.js
+++ server/server.js
@@ -2,10 +2,25 @@ const express = require('express');
 const dotenv = require("dotenv");
 const blogRoutes=require('./routes/blogRoutes')
 
+
 const connectDB = require("./config/db");
-const app = express();
+const app =express();
 dotenv.config();
 app.use(express.json());
+
+connectDB();
+
+const PORT = 3001 || 3002;
+app.listen(PORT, console.log(`server started on port ${PORT}`));
+
+
+//app.use(express.json());
+app.use("/api/blogs",blogRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+console.log(process.env.MONGO_UR);
 
 connectDB();
 const middleware = (req, res, next) => {
@@ -13,13 +28,10 @@ const middleware = (req, res, next) => {
 }
 middleware();
 
-const PORT = 3001 || 3002;
 app.listen(PORT, console.log(`server started on port ${PORT}`));
 
 const User = require('./models/userSchema')
 
-//app.use(express.json());
-app.use("/api/blogs",blogRoutes);
 app.use(require('./router/auth'))
 
 app.use(express.json());
