832567980e3b51635a8c5640960f8ce8d2b46ace
unknown <mahekmj123@gmail.com>
2023-03-05 03:24:02 +0530

    ui improved

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-poppins '>
-      <footer className="footer p-10 bg-base-200 text-base-content">
+    <div className='text-poppins mx-auto'>
+      <footer className="footer p-10 bg-dark text-base-content text-center">
   
-  <div>
-    <span className="footer-title">TRAVMATE</span> 
-    <a className="link link-hover">Explore</a> 
-    <a className="link link-hover">Blogs</a>
-    <a className="link link-hover">About us</a> 
-    <a className="link link-hover">Contact</a> 
+  <div className='md:mx-auto'>
+    <span className="footer-title text-2xl md:text-3xl">TRAVMATE</span> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Explore</a> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Blogs</a>
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">About us</a> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Contact</a> 
   </div> 
-  <div>
-    <span className="footer-title">Legal</span> 
-    <a className="link link-hover">Terms of use</a> 
-    <a className="link link-hover">Privacy policy</a> 
-    <a className="link link-hover">Cookie policy</a>
+  <div className='md:mx-auto'>
+    <span className="footer-title text-2xl md:text-3xl">Legal</span> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Terms of use</a> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Privacy policy</a> 
+    <a className="link link-hover text-xl md:text-2xl text-primary font-poppins">Cookie policy</a>
   </div> 
-  <div>
-    <span className="footer-title">Newsletter</span> 
+  <div className='md:mx-auto'>
+    <span className="footer-title text-2xl md:text-3xl">Newsletter</span> 
     <div className="form-control w-80">
       <label className="label">
-        <span className="label-text font-poppins p-2">Enter your email address</span>
+        <span className="label-text font-poppins p-2 md:text-xl text-lg text-primary">Enter your email address</span>
       </label> 
       <div className="relative">
-        <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" /> 
-        <button className="btn btn-gray-900 absolute top-0 right-0 rounded-l-none">Subscribe</button>
+        <input type="text" placeholder="youremail@gmail.com" className="input input-bordered w-full pr-16" /> 
+        <button className="btn btn-gray-900 absolute top-0 right-0 rounded-l-none bg-mint text-dark">Subscribe</button>
       </div>
     </div>
   </div>
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -20,7 +20,7 @@ const About = () => {
     <div class=" bg-[#8297b8] "> 
    
    <div className="h-[500px] flex justify-center items-center bg-aboutbg bg-fixed bg-contain bg-no-repeat">
-          <p className=" text-white md:text-3xl lg:text-5xl  text-center font-semibold mt-20 w-full font-playfair">
+          <p className=" text-mint md:text-3xl lg:text-5xl  text-center font-semibold mt-20 w-full font-playfair">
          
     Going Places
       I haven’t been everywhere,<br/> but it’s on my list
@@ -30,13 +30,10 @@ const About = () => {
      
       
             <div class="p-20">
-              <h2 class="text-2xl text-gray-900 font-bold md:text-4xl">Our team consists of experienced travel experts who have a deep understanding of different destinations and cultures. </h2>
-              <p class="mt-6 text-gray-600">Welcome to our travel company! We are a team of passionate travel enthusiasts dedicated to helping our clients create unforgettable travel experiences. Whether you're looking to explore the natural wonders of the world, immerse yourself in different cultures, or simply unwind and relax in a beautiful setting, we've got you covered.
-
-We believe that travel is more than just seeing new places - it's about connecting with new people, experiencing different ways of life, and creating memories that will last a lifetime. That's why we work hard to craft unique itineraries that cater to your interests, preferences, and budget.
-We pride ourselves on our attention to detail, ensuring that every aspect of your trip is carefully planned and executed to perfection.
-
-</p>
+              <h2 class="text-2xl text-dark font-bold md:text-4xl font-poppins">Our team consists of experienced travel experts who have a deep understanding of different destinations and cultures. </h2>
+              <p class="mt-6 text-dark font-poppins text-xl">
+                Welcome to our travel company! We are a team of passionate travel enthusiasts dedicated to helping our clients create unforgettable travel experiences. Whether you're looking to explore the natural wonders of the world, immerse yourself in different cultures, or simply unwind and relax in a beautiful setting, we've got you covered. We believe that travel is more than just seeing new places - it's about connecting with new people, experiencing different ways of life, and creating memories that will last a lifetime. That's why we work hard to craft unique itineraries that cater to your interests, preferences, and budget.We pride ourselves on our attention to detail, ensuring that every aspect of your trip is carefully planned and executed to perfection.
+              </p>
           
          
           </div>
