e9c710744e44ecea1361cc0638d072b25231e419
Rhythm <rhythmshah21@gmail.com>
2023-03-05 10:18:29 +0530

    home

--- client/src/components/TravMates.jsx
+++ client/src/components/TravMates.jsx
@@ -8,18 +8,18 @@ const TravMates = () => {
     <div className="w-4/5 mx-auto">
     <div className="text-center pt-44 pb-16 ">
       <h1 className="text-4xl">
-        <b>UPCOMING TOURS</b>
+        <b>TRAVEL BUDDIES</b>
       </h1>
     </div>
     <div className="h-0.5  bg-gray-300">
          </div>
     <div className="flex justify-between">
         <div>
-        <p className="pt-8 text-2xl font-medium">Jaipur Tour</p>
-        <p className="pt-2">The Pink City, Rajasthan</p>
+        <p className="pt-8 text-2xl font-medium">RHYTHM</p>
+        <p className="pt-2">Mumbai,Maharastra</p>
         </div>
         <div className="pt-4">
-       <Link to={`/attraction/${id}`}> <button class="bg-black text-white w-36 h-12 rounded-2xl mt-8 hover:bg-red-900 font-poppins">Find Mates</button></Link>
+       <Link to={`/attraction/${id}`}> <button class="bg-black text-white w-36 h-12 rounded-2xl mt-8 hover:bg-red-900 font-poppins">CONNECT</button></Link>
         </div>
         
     </div>
